e0b87361ec9ca8de4db6500a4ef5c37685a8f72a
Mateusz <mwasylow@gmail.com>
2025-09-09 08:59:22 +0200

    playing around

--- dbt/models/staging/stg_customers.sql
+++ dbt/models/staging/stg_customers.sql
@@ -1,9 +1,5 @@
 
 {{ config(materialized='view') }}
 
-select
-  cast(id as int64) as customer_id,
-  initcap(name) as customer_name,
-  lower(email) as email,
-  cast(signup_date as date) as signup_date
+
 from {{ source('raw', 'excersise_2_customers') }}
